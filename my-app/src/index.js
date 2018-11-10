c8c9ed37a5a1a28bed68b0e053ec62cf4601b42a
Shruti Nanajkar <nanajkar.s@husky.neu.edu>
2018-11-10 08:53:12 -0500

    passing value from one component to another(Board to Square)

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
 class Square extends React.Component {
     render() {
       return (
         <button className="square">
-          {/* TODO */}
+          {this.props.value}
         </button>
       );
     }
@@ -10,7 +15,7 @@ class Square extends React.Component {
   
   class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+      return <Square value={i} />;
     }
   
     render() {
